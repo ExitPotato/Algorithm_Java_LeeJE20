b95deb04eb309acaddca4fdc192009ee75083fea
이정은 <silver36c@gmail.com>
2023-08-14 14:31:01 +0900

    feat: SWEA 코드 베이스

---
+++ base/Solution.java
@@ -0,0 +1,54 @@
+package base;
+
+/*
+[링크]
+
+[시간]
+
+
+[아이디어]
+
+[시간복잡도]
+
+
+[실수]
+
+
+[검색]
+
+
+[다른 사람 코드]
+
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int T, N, M;
+	static int[][] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			sb.append("#").append(t).append(" ");
+
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			arr = new int[N][M];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j <	M; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+		}
+		// solve
+	}
+}
