aff2c51aaa7fdb47e418163ef748f23cb4091590
이정은 <silver36c@gmail.com>
2023-09-25 15:34:30 +0900

    feat: hello world

---
+++ base/HelloWorld.java
@@ -0,0 +1,52 @@
+package base;
+
+/*
+[링크]
+
+
+[시간]
+
+
+[아이디어]
+
+
+[시간복잡도]
+
+
+[실수]
+
+
+[검색]
+
+
+[다른 사람 코드]
+
+
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int K, N, M;
+		int[][] arr;
+		K = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+}
